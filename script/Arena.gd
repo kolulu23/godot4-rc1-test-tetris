242e0e2d9d559f9a9b388fabd55c6ee17e008ce5
kolulu <kolulu28@outlook.com>
2023-02-16 23:30:18 +0800

    Basic movement

--- script/Arena.gd
+++ script/Arena.gd
@@ -1,23 +1,23 @@
 extends Node2D
 
 var active_tetromino: Tetromino
+
+# One dimension array for our 20 * 10 cells arena
 var arena_cells: Array[TetroBlock]
 
 const ARENA_WIDTH: int = 10
 const ARENA_WIDTH_PIXEL: float = ARENA_WIDTH * TetroBlock.BLOCK_SIZE
 const ARENA_HEIGHT: int = 20
 const ARENA_HEIGHT_PIXEL: float = ARENA_HEIGHT * TetroBlock.BLOCK_SIZE
+
+## The initial tetromino spawn point is in the top middle of this arena
 const SPAWN_POINT: Vector2 = Vector2(ARENA_WIDTH_PIXEL / 2.0, TetroBlock.BLOCK_SIZE)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	# TODO
-	active_tetromino = $TetrominoSpawner.spawn_random()
-	self.add_child(active_tetromino)
-	active_tetromino.position = SPAWN_POINT
-	print("[1]Spawn ", active_tetromino)
+	self.setup_active_tetromino()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,15 +35,34 @@ func try_move_tetromino(event: InputEvent) -> void:
 	# TODO Set a custome timeout to action hold time
 	if event.is_action_pressed("tetro_move_left", true):
 		print("todo, move left ", active_tetromino)
+		active_tetromino.move_left()
 	if event.is_action_pressed("tetro_move_right", true):
 		print("todo, move right ", active_tetromino)
+		active_tetromino.move_right()
 	if event.is_action_pressed("tetro_soft_drop", true):
 		print("todo, soft drop ", active_tetromino)
+		# Stop and restart the timer to avoid accelaration
+		$TetrominoDropTimer.stop()
+		active_tetromino.move_down()
+		$TetrominoDropTimer.start()
 	if event.is_action_pressed("tetro_hard_drop"):
 		print("todo, hard drop ", active_tetromino)
 	if event.is_action_pressed("tetro_rotate_cw"):
 		print("todo, rotate clockwise ", active_tetromino)
+		active_tetromino.rotate_cw()
 	if event.is_action_pressed("tetro_rotate_ccw"):
 		print("todo, rotate counter-clockwise ", active_tetromino)
+		active_tetromino.rotate_ccw()
 	if event.is_action_pressed("tetro_hold"):
 		print("todo, hold ", active_tetromino)
+
+
+func setup_active_tetromino():
+	active_tetromino = $TetrominoSpawner.spawn_random()
+	self.add_child(active_tetromino)
+	active_tetromino.position = SPAWN_POINT
+	print("[1]Spawn ", active_tetromino)
+
+
+func _on_tetromino_drop_timer_timeout():
+	active_tetromino.move_down()
--- script/Tetromino.gd
+++ script/Tetromino.gd
@@ -5,6 +5,7 @@ enum TetroType { I, J, L, O, S, T, Z }
 
 var tetro_type: TetroType
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass  # Replace with function body.
@@ -13,3 +14,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func move_left():
+	self.move_to(Vector2.LEFT)
+
+
+func move_right():
+	self.move_to(Vector2.RIGHT)
+
+
+func move_down():
+	self.move_to(Vector2.DOWN)
+
+
+## Move to [code]direction[/code] relative to this node's parent
+func move_to(direction: Vector2):
+	self.position += TetroBlock.BLOCK_SIZE * direction
+
+
+## Rotate self clockwise
+func rotate_cw():
+	self.rotation += PI / 2
+
+
+## Rotate self counter-clockwise
+func rotate_ccw():
+	self.rotation -= PI / 2
